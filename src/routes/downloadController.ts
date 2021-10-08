02f1084432876c2454d2b3d46cd17b623521c791
Arkady Sheglov <a.sheglov@securitycode.ru>
2021-10-08 18:25:36 +0300

    Поправил отправку файлов с учетом symlink

--- src/routes/downloadController.ts
+++ src/routes/downloadController.ts
@@ -1,7 +1,7 @@
-import {Request, Response} from "express";
-import {NextFunction} from "express-serve-static-core";
-import * as fs from "fs";
-import path from "path";
+import { Request, Response } from 'express';
+import { NextFunction } from 'express-serve-static-core';
+import * as fs from 'fs';
+import path from 'path';
 
 /**
  * Папка, где лежат разрешенные файлы для скачивания
@@ -16,36 +16,75 @@ const folder = path.normalize(path.resolve('_download'));
  */
 export const handleDownload = (request: Request, response: Response, next: NextFunction) => {
 
-    // Создали папку по необходимости
-    if (!fs.existsSync(folder)) {
-        fs.mkdirSync(folder);
+  // Создали папку по необходимости
+  if (!fs.existsSync(folder)) {
+    console.log('creating folder');
+    fs.mkdirSync(folder);
+  }
+
+  const file: string = request.query['file'] as string;
+  console.log('origin file from request: ' + file);
+
+  // нет файла, выходим
+  if (!file) {
+    console.log('No file provided');
+    response.status(403)
+      .send('No file provided');
+    return;
+  }
+
+  // файлы должны лежать в папке _download
+  const filepath: string = path.normalize(path.resolve(folder, file));
+  console.log('filename: ' + filepath);
+
+  if (!fs.existsSync(filepath)) {
+    console.log(`this file [${file}] is not existing`);
+    response.status(404)
+      .send(`this file [${file}] is not existing`);
+    return;
+  }
+
+  // нацелили файл выше папки download, защищаемся
+  if (!filepath.includes(folder)) {
+    console.log('outside of download folder');
+    response.status(403)
+      .send('outside of download folder');
+    return;
+  }
+
+  const clientFileName: string = path.basename(filepath);
+
+  fs.realpath(filepath, (err, resolvedPath) => {
+    if (err) {
+      console.log(err);
+      response.status(500).send(err);
+    } else {
+      response.download(resolvedPath, clientFileName);
     }
+  });
 
-    const file: string = request.query['file'] as string;
-    // нет файла, выходим
-    if (!file) {
-        response.status(403)
-            .send('No file provided');
-        return;
-    }
-
-    // файлы должны лежать в папке _download
-    const filepath: string = path.normalize(path.resolve(folder, file));
-    console.log('filename: ' + filepath);
-
-    if (!fs.existsSync(filepath)) {
-        response.status(404)
-            .send(`this file [${file}] is not existing`);
-        return;
-    }
-
-    // нацелили файл выше папки download, защищаемся
-    if (!filepath.includes(folder)) {
-        response.status(403)
-            .send('outside of download folder');
-        return;
-    }
-
-    const clientFileName: string = path.basename(filepath);
-    response.download(filepath, clientFileName);
-}
+  // читаю статы файла
+  // fs.lstat(filepath, (err, stats) => {
+  //   if (err) {
+  //     console.log(err);
+  //     response.status(500).send(err);
+  //   } else {
+  //     // если символ. ссылка
+  //     if (stats.isSymbolicLink()) {
+  //       // узнаю источник
+  //       fs.readlink(filepath, (err1, linkString) => {
+  //         if (err1) {
+  //           console.log(err1);
+  //           response.status(500).send(err1);
+  //         } else {
+  //           // посылаю источник
+  //           response.download(linkString, clientFileName);
+  //         }
+  //       });
+  //     } else {
+  //       // отсылаю сам файл
+  //       response.download(filepath, clientFileName);
+  //     }
+  //   }
+  // });
+};
