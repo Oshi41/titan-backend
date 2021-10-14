a1aad715d1cfd05be2de2925439dc4bb1fc7e9cb
oshi4 <sheglovaoe@gmail.com>
2021-10-14 15:39:28 +0300

    Поправил создание папки с crash reports

--- src/init/download.ts
+++ src/init/download.ts
@@ -2,7 +2,9 @@ import path from "path";
 import {createPath} from "../utils/index";
 
 export const DownloadFolder: string = path.resolve('_download');
+const reportsFolder: string = path.resolve('_storage', 'crash reports');
 
 export const checkDownloadFolder = async () => {
     await createPath(DownloadFolder);
+    await createPath(reportsFolder);
 }
